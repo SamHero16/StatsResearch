85c631c7194fe2dc084269572a17447e33fa5bec
Sam Herold <samherold@Sams-MacBook-Pro.local>
2023-06-12 13:53:43 -0700

    Base simulation touch ups. Added confidence intervals for point estimate and MSE, both in ways that could likely be incorrect. For Point estimate, took mean of all upper bounds and lower bounds and plotted. I did this in kind of a sloppy way. For MSE, used chisquared something or other which I found online. Also cleaned up plots and ran two simulations with 300 TPRS and 15 loops, I included those on this commit. Also included saving features but need help with the details on those.

--- Simulation1.R
+++ Simulation1.R
@@ -1,113 +1,156 @@
-  ### Simulation 1: SLR
-  
-  library(spectralGP)
-  library(mgcv)
-  library(broom)
-  library(ggplot2)
-  library(dplyr)
-  
-  ##Settings##
-  
-  #Fully adjustable
-  
-  L = 5 ## Number of samples taken
-  
-  N = 16384 ## Sample Size
-  
-  M = 50 ## Number of TPRS 
-  
-  ## Desired Outcomes 
-  b0 = 0
-  b1 = 1
-  
-  D = 512 ## Dimension of Population
-  
-  
-  ## Gaussian Process, c(.1,2) set arbitrarily
-  gp=gp(c(D,D),matern.specdens,c(.1,2))
-  simulate(gp)
-  
-  ##Confounding surface
-  confounder = gp(c(D,D),matern.specdens,c(.15,2))
-  simulate(confounder)
-  
-  
-  ##Setup TPRS: Create the full set, then increment later using loops
-  ##TPRS are size of population, will take sample later
-  o1 = gp$omega$omega1
-  o2 = gp$omega$omega2
-  o1o2 = data.frame(o1,o2)
-  smoother = s(o1,o2,k = M)
-  Smoothframe = smooth.construct(smoother, data = o1o2, knots = NULL)
-  TPRS = data.frame(Smoothframe$X)
-  
-  ##rearrange columns for easy looping
-  TPRS <- TPRS %>%
-    select(M-2:M, everything())
-  
-  ##Create population
-  WD = D*D
-  popx = gp$process + .5*confounder$process
-  popy = popx + .5*confounder$process + rnorm(WD,0,1)
-  
-  #############
-  
-  #Matrix for storage
-  mat <- matrix(ncol = M -2)
-  
-  
-  
-  #Simulation Loop: Sample data, try 3:10 TPRS's, record MSE.
-  
-  for(i in 1:L){
-    ##Create Sample set: important because data is related by location
-    sample = sample(1:WD,16384)
-    ##Create sample
-    x = popx[sample]
+    ### Simulation 1: SLR
     
-    ##Create sample
+    library(spectralGP)
+    library(mgcv)
+    library(broom)
+    library(ggplot2)
+    library(dplyr)
     
-    y = popy[sample]
+    ##Settings##
     
     
-    ##Create results vector for this Sample
-    vecResults = c()
     
-   
-    ##Loop through different number of TPRS
-    for(i in 3:M){
-      workingSplines = data.matrix(TPRS[sample,1:i]) #Switched back to matrix because data frame wasnt working
+    
+    
+    L = 10 ## Number of samples taken
+    
+    N = 1000 ## Sample Size
+    
+    M = 50 ## Number of TPRS 
+    
+    ## Desired Outcomes 
+    b0 = 0
+    b1 = 1
+    
+    D = 512 ## Dimension of Population
+    
+    
+    ## Gaussian Process, c(.1,2) set arbitrarily
+    gp=gp(c(D,D),matern.specdens,c(.1,2))
+    simulate(gp)
+    
+    ##Confounding surface// change first entry of vector for adjustment
+    confounder = gp(c(D,D),matern.specdens,c(.5,2))
+    simulate(confounder)
+    
+    
+    ##Setup TPRS: Create the full set, then increment later using loops
+    ##TPRS are size of population, will take sample later
+    o1 = gp$omega$omega1
+    o2 = gp$omega$omega2
+    o1o2 = data.frame(o1,o2)
+    smoother = s(o1,o2,k = M+1)
+    Smoothframe = smooth.construct(smoother, data = o1o2, knots = NULL)
+    TPRS = data.frame(Smoothframe$X)
+    
+    ##rearrange columns for easy looping
+    TPRS <- TPRS[,c(M:(M+1),1:(M-2))]
+    
+    ##Create population
+    WD = D*D
+    popx = gp$process + .5*confounder$process
+    popy = popx + .5*confounder$process 
+    
+    #############
+    
+    #Matrix's for storage
+    mat <- matrix(nrow = L, ncol = M - 2)
+    CI <- matrix(0,nrow = 2, ncol = M-2)
+    colnames(CI) = c(3:M)
+    
+    
+    #Simulation Loop: Sample data, try 3:10 TPRS's, record MSE.
+    
+    for(i in 1:L){
+      ##Create Sample set: important because data is related by location
+      sample = sample(1:WD,N)
+      ##Create sample
+      x = popx[sample]
       
-      #Fit Linear Regression Model and collect results
-      model = tidy(lm(y~x + workingSplines) )
-      vecResults = append(vecResults, model$estimate[2]) 
+      ##Create sample
+      
+      y = popy[sample] + rnorm(N,0,1)
+      
+      
+      ##Create results vector for this sample
+      observation = numeric(M-2)
+      
+      
+      
+      z = 1
+      ##Loop through different number of TPRS
+      for(j in 3:M){
+        workingSplines = data.matrix(TPRS[sample,1:j]) #Switched back to matrix because data frame wasnt working
+        #Fit Linear Regression Model and collect results
+        model = lm(y~x + workingSplines)
+        observation[j-2] = (tidy(model)$estimate[2])
+        tempCI = tidy(confint(model, level=0.95))
+        
+        ## First row is lower bound and second row is upper bound
+        CI[z] = CI[z] + tempCI$x[,"2.5 %"][2]
+        CI[z+1]= CI[z+1] + tempCI$x[,"97.5 %"][2]
+        z = z + 2
+        
+      }
+      
+    ##Add data to matrix 
+    mat[i,] = observation
+    
+     
+     
+     
+    }
+    #Rename columns for clarity
+
+    colnames(mat) = c(3:M)
+    
+    #Find average of the CI's
+    CI = CI/L
+    
+    
+    
+    ##MSE CALC
+    MSEbynumspline = apply(mat,2,function(w) sqrt(mean((w-b1)^2)))
+   
+    
+    #Resulting Information and loose summary of findings
+    
+      #Finding CI on MSE, dont know if this is correct
+    
+    rmse_interval <- function(rmse, deg_free, p_lower = 0.025, p_upper = 0.975){
+      tibble(.pred_lower = sqrt(deg_free / qchisq(p_upper, df = deg_free)) * rmse,
+             .pred_upper = sqrt(deg_free / qchisq(p_lower, df = deg_free)) * rmse)
     }
     
-  ##Add data to matrix 
-  mat = rbind(mat,vecResults)
-   
-   
-   
-  }
-  #Take off first useless row
-  mat = mat[-1,]
+    rmseint = rmse_interval(MSEbynumspline,M)
+    
+    
+    
+    ggplot() + 
+      geom_point(aes(x = 3:M,y = MSEbynumspline),size = .75) +
+      geom_point(aes(x = 3:M, y = rmseint$.pred_lower,colour = "red"),size = .1) +
+      geom_point(aes(x = 3:M, rmseint$.pred_upper,colour = "red"),size = .1) + 
+      labs( x = "TPRS degrees of freedom", y = "Mean Squared Error")
+    
+    ##ggsave("/Users/samherold/Desktop/StatsResearch/MSEplot.csv")
+    
+    
+    ggplot() + geom_point(aes(x = 3:M,y = colMeans(mat)),size = .5) + geom_path(aes(3:M,colMeans(mat)))+
+      geom_point(aes(x = 3:M, y = CI[1,],colour = "red"),size = .5) + geom_path(aes(3:M,CI[1,],colour = "red"))+
+      geom_point(aes(x = 3:M, y = CI[2,],colour = "red"),size = .5 ) + geom_path(aes(3:M,CI[2,],colour = "red"))+
+      labs( x = "TPRS degrees of freedom", y = "Point estimate") + 
+      geom_hline(yintercept = 1)
+    
+    ##ggsave("/Users/samherold/Desktop/StatsResearch/PointEstimatePlot.csv")
   
+    
+    ##Saving results 
+    
+      #####How do I add date to this? paste0 was acting wierd..
+      ##why is my filepath wrong
+    ##write.csv(mat,file="/Users/samherold/Desktop/StatsResearch/Estimates.csv")
+    
+    
+    
   
-  ##MSE CALC
-  MSEbynumspline = c()
-  for(i in 1:ncol(mat)){
-    mse = mean((mat[,i] - 1)^2)
-    MSEbynumspline[i] = mse
-  }
-  
-  #Resulting Information and loose summary of findings
-  lm(y~x)
-  MSEbynumspline
-  plot(colMeans(mat))
-  plot(MSEbynumspline)
-  min(colMeans(mat))
-  min(MSEbynumspline)
-  
-  
-#propose to Study actual time effectiveness of creating the splines as side project. 
-  
+    
